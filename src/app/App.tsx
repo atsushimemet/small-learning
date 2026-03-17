0b1649b7f87b1dc32ca97eddbf4b73ca41cfe4ad
atsushimemet <noap3b69n@gmail.com>
2026-03-17 15:12:39 +0900

    add: vercel(react) analytics

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,13 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { Toaster } from "./components/ui/sonner";
 import { LearningLogServiceProvider } from "./services/learningLogService";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <LearningLogServiceProvider>
       <RouterProvider router={router} />
+      <Analytics />
       <Toaster position="top-center" />
     </LearningLogServiceProvider>
   );
